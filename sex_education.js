f645728b8041054e94d60253692a4301d9d8dda7
Gediminas <89471847+GedasK@users.noreply.github.com>
2023-06-02 12:38:43 +0300

    Update sex_education.js

--- sex_education.js
+++ sex_education.js
@@ -1,4 +1,4 @@
-    (function() {
+        (function() {
         function async_load(target){
             var s = target.document.createElement('script');
             s.type = 'text/javascript';
@@ -18,6 +18,11 @@
           async_load(window)
         }
     })();
+    
+document.querySelector('# ').addEventListener('click', function() {
+	const openBubble = new MouseEvent('click');
+	document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+});
     
 var clickFunction = function() {
 
